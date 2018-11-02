847611f81a61ec4ef18f67e22cb869468a1ba75a
Eduardo Gouveia <eduardogouveia4@gmail.com>
2018-11-02 12:22:05 +0000

    Agora vai

--- server.php
+++ server.php
@@ -19,5 +19,9 @@ if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
 }
 
 sdsadsasadsadsadsadsadasdsadasdsadsa
+sdsadsasadsadsadsadsadasdsadasdsadsaad
+sad
+asd
+asddas
 
 require_once __DIR__.'/public/index.php';
